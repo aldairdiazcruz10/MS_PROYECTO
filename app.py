8afb634d798f333334418e7e4fdfc26596a11ebe
aldairdiazcruz10 <103942354+aldairdiazcruz10@users.noreply.github.com>
2025-10-15 11:06:12 -0500

    app.py

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def create_app():
     app.register_blueprint(mensajes_bp)
     app.register_blueprint(cumple_docente_bp)
     return app
-    
-    
+
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)                                                                                                                                                                                                                                                                                                                                                 
