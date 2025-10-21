a1bb8964291003ab0e7d4b03b553030351c9c0a2
aldairdiazcruz10 <103942354+aldairdiazcruz10@users.noreply.github.com>
2025-10-20 19:02:28 -0500

    app.py

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ def create_app():
     app = Flask(__name__)
     
     # Configuración de Mail usando variables de entorno
+    # Configuración de Mail (Brevo)
     app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = os.environ.get('mailto:99b357001@smtp-brevo.com')
-    app.config['MAIL_PASSWORD'] = os.environ.get('cpTLABJKPS8z6D14')
-    app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('eduardo.diaz@mastersystem.edu.pe')
+    app.config['MAIL_USERNAME'] = '99b357001@smtp-brevo.com'  # tu identificador Brevo
+    app.config['MAIL_PASSWORD'] = 'cpTLABJKPS8z6D14'          # tu contraseña SMTP de Brevo
+    app.config['MAIL_DEFAULT_SENDER'] = '99b357001@smtp-brevo.com'
 
     # Inicializar Mail
     mail.init_app(app)
